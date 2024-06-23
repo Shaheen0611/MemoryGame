36cd407ee259a84c400a16364b87c78fc6695ba8
Shaheen Thayalan <shaheen@Shaheens-MacBook-Pro.local>
2024-06-23 23:30:27 +0200

    made the cards flipbut the animation needs to be better also i have made it so that the cards can be selected and be remembered if they are right

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MemoryGame
+This is something i have done in my free time to increase my coding experience.
--- elemental-memory/src/App.js
+++ elemental-memory/src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SingleCard from "./component/SingleCard";
 
 const cardImages = [
-  { src: "/Img/Air.png" },
-  { src: "/Img/Dark.png" },
-  { src: "/Img/Earth.png" },
-  { src: "/Img/Fire.png" },
-  { src: "/Img/Light.png" },
-  { src: "/Img/Water.png" },
+  { src: "/Img/Air.png", matched: false },
+  { src: "/Img/Dark.png", matched: false },
+  { src: "/Img/Earth.png", matched: false },
+  { src: "/Img/Fire.png", matched: false },
+  { src: "/Img/Light.png", matched: false },
+  { src: "/Img/Water.png", matched: false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // SHUFFLE CARDS
   const shuffleCards = () => {
@@ -25,14 +27,52 @@ function App() {
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  // HANDLE A CHOICE
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  //COMPARE TWO SELECTED CARDS
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  // RESET CHOICES AND INCREASE TURN
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
   return (
     <div className="App">
       <h1>Elemental Match</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
